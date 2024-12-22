c12672d280d5892b74a6808c8c095874d8a15510
Le Kien Truc <34625840+lktruc@users.noreply.github.com>
2024-12-22 13:45:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,103 +1,117 @@
-#Use the ResNet18 on Cifar-10
 import torch
+import torchvision
+import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-import argparse
-import os
 from resnet18 import ResNet18
 from resnet import Bottleneck, ResNet, ResNet50, ResNet101
 
 #check gpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('device: ', device)
 
-#set hyperparameter
-EPOCH = 10
-pre_epoch = 0
-BATCH_SIZE = 128
-LR = 0.01
-
-#prepare dataset and preprocessing
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
 
-#labels in CIFAR10
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+trainloader = torch.utils.data.DataLoader(train, batch_size=128, shuffle=True, num_workers=2)
 
-#define ResNet18
-net = ResNet18().to(device)
-# net = ResNet50(10).to(device)
-# net = ResNet101(10).to(device)
+test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 
-#define loss funtion & optimizer
+testloader = torch.utils.data.DataLoader(test, batch_size=128,shuffle=False, num_workers=2)
+
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+net = ResNet50(10).to('cuda')
+# net = ResNet18().to(device)
+net_t = ResNet18().to(device)
+net_t.load_state_dict(torch.load('best_resnet18_v2_state_dict.pth'))
+net_t.eval()
+
+
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4)
+optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = 0.1, patience=5)
 
-
-#train
+EPOCHS = 100
 best_correct = 0
-for epoch in range(pre_epoch, EPOCH):
-    print('\nEpoch: %d' % (epoch + 1))
-    net.train()
-    sum_loss = 0.0
-    correct = 0.0
-    total = 0.0
-    for i, data in enumerate(trainloader, 0):
-        #prepare dataset
-        length = len(trainloader)
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
+accuracy_list = []
+
+for epoch in range(EPOCHS):
+    losses = []
+    kd_loss_arr = []
+    running_loss = 0
+    running_kd_loss = 0
+    for i, inp in enumerate(trainloader):
+        inputs, labels = inp
+        inputs, labels = inputs.to('cuda'), labels.to('cuda')
         optimizer.zero_grad()
-
-        #forward & backward
+    
         outputs = net(inputs)
+        output_t = net_t(inputs)
+        alpha = 3
+        T = 8  # temperature
+        kd_loss = 0.0
+        kd_loss = F.kl_div(F.log_softmax(outputs/T, dim=1),
+                            F.softmax(output_t/T, dim=1), reduction='batchmean') * alpha
+
         loss = criterion(outputs, labels)
-        loss.backward()
+        losses.append(loss.item())
+        kd_loss_arr.append(kd_loss.item())
+        # loss.backward()
+        (loss + kd_loss).backward()
         optimizer.step()
+        
+        running_loss += loss.item()
+        running_kd_loss += kd_loss.item()
+        if i%100 == 0 and i > 0:
+            print(f'Loss [{epoch+1}, {i}](epoch, minibatch): ', running_loss / 100)
+            running_loss = 0.0
 
-        #print ac & loss in each batch
-        sum_loss += loss.item()
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += predicted.eq(labels.data).cpu().sum()
-        if i % 50 == 0:
-          print('[epoch:%d, iter:%d] Loss: %.03f | Acc: %.3f%% '
-              % (epoch + 1, (i + 1 + epoch * length), sum_loss / (i + 1), 100. * correct / total))
+            print(f'KD Loss [{epoch+1}, {i}](epoch, minibatch): ', running_kd_loss / 100)
+            running_kd_loss = 0.0
 
-    #get the ac with testdataset in each epoch
-    print('Waiting Test...')
+    avg_loss = sum(losses)/len(losses)
+    scheduler.step(avg_loss)
+
+    avg_kd_loss = sum(kd_loss_arr)/len(kd_loss_arr)
+    scheduler.step(avg_kd_loss)
+
+    correct = 0
+    total = 0
     with torch.no_grad():
-        correct = 0
-        total = 0
         for data in testloader:
-            net.eval()
             images, labels = data
-            images, labels = images.to(device), labels.to(device)
+            images, labels = images.to('cuda'), labels.to('cuda')
             outputs = net(images)
+            
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
-            correct += (predicted == labels).sum()
+            correct += (predicted == labels).sum().item()
     if best_correct < correct:
         best_correct = correct
-        torch.save(net.state_dict(), 'best_resnet18_state_dict.pth')
+        torch.save(net.state_dict(), 'best_resnet50_kd_8_3_state_dict.pth')
         print('Model saved with accuracy: {:.2f}%'.format(100*(best_correct/total)))
-    print('Test\'s ac is: %.3f%%' % (100 * correct / total))
+        
 
-print('Train has finished, total epoch is %d' % EPOCH)
+    print('Accuracy on 10,000 test images: ', 100*(correct/total), '%')
+    accuracy_list.append(round(100*(correct/total), 2))
+            
+print('Training Done')
+import csv
+with open('test_resnet50_kd_8_3', 'w') as f:
+     
+    write = csv.writer(f)
+    write.writerow(accuracy_list)
